69194f4d99ea555684a49738ee48533566c3af8c
benhar <charles@ictact.co.in>
2018-03-04 17:54:24 +0530

    Shoping Project

---
+++ includes/footer-btm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	
+</head>
+<body>
+	<div class="footer-btm">
+		<p style="color: blue;">Developped by Md. Shahjalal || &copy; 2018</p>
+	</div>
+
+	<!-- <style type="text/css" media="screen">
+		.footer-btm{
+					text-align: center;
+					font-size: 24px;
+					font-family: arial;
+					padding: 30px;
+					position: fixed;
+					color: blue;
+				  }
+	</style> -->
+</body>
+</html>
